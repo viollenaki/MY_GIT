7418e9091b177be2f0e7131dd7295da89b378ab1
viollenaki <tanakaakuustik@gmail.com>
2024-12-03 13:44:08 +0600

    update

--- daniyar.py
+++ daniyar.py
@@ -1,34 +1,14 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QMainWindow
-from PyQt5.QtCore import Qt
-
-
-class MyApp(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("My App")
-        self.setGeometry(450, 300, 400, 300)
-
-        self.label = QLabel("Are you gay?", self)
-        self.button1 = QPushButton('yes', self)
-        
+from flask import Flask, request, jsonify
 
 
 
+app = Flask(__name__)
+@app.route('/daniya')
+def daniya():
+    return 'ehg'
 
-        self.label.move(150, 150)
+@app.route('/dan')
+def dsf():
+ return 'asdf'
 
-        self.show()
-
-    def no_button(self):
-        self.button2 = QPushButton('no', self)
-        self.
-
-def main():
-    app = QApplication(sys.argv)
-    window = MyApp()
-    window.show()
-    sys.exit(app.exec_())
-
-if __name__ == "__main__":
-    main()
+app.run()
